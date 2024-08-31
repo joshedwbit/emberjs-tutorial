ce7da79fb3211154cb7434e59e382689fe0500f9
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-08-31 23:22:29 +0100

    dynamic routing

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class ItemRoute extends Route {}
+export default class ItemRoute extends Route {
+    model(params) {
+        const {
+            item_id
+            // assigns item id in the url to a const item_id
+        } = params;
+        return item_id;
+    }
+}
